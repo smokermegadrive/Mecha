4eb5b229c9dbb1e4d232085d0ccc0ea610787291
Agustin Paoletti <agustinpaoletti92@outlook.com>
2020-05-20 19:00:01 -0300

    Tipo de mecha agregado en #2

--- src/mechadecorator/Client.java
+++ src/mechadecorator/Client.java
@@ -5,6 +5,10 @@
  */
 package mechadecorator;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Random;
 
 /**
@@ -16,7 +20,7 @@ public class Client {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
        /* Mecha sword = new Sword(new Base());
         sword.equip();
@@ -28,20 +32,34 @@ public class Client {
        Random random = new Random();
        int select = random.nextInt(arr.length);
        
-       Mecha mecha = MechaFactory.createMecha("SCOPEDOG");
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       System.out.println("Escriba tipo de mecha");
+       String tipo = br.readLine();
+       
+       while (tipo.equals("") || !Arrays.asList(arr).contains(tipo)) {
+           System.out.println("Seleccione un tipo valido");
+           tipo = br.readLine();
+       }
+       
+       Mecha mecha = MechaFactory.createMecha(tipo);
        Mecha mecha2 = MechaFactory.createMecha(arr[select]);
+       
+       System.out.println("Escriba su nombre");
+       String nombre = br.readLine();
+       mecha.setName(nombre);
+       
        while (mecha.estasVivo() && mecha2.estasVivo()) {
         mecha.atacar(mecha2);
         System.out.println(mecha2.getName() + " Hp: " + mecha2.getHp());
         
         if (mecha2.estasVivo()) {
             mecha2.atacar(mecha);
-            System.out.println(mecha.getName() + " Hp: " + mecha.getHp());
+            System.out.println(nombre + " Hp: " + mecha.getHp());
         }
         }
        
        if (!mecha.estasVivo()) {
-           System.out.println(mecha.getName() + " derrotado");
+           System.out.println(nombre + " derrotado");
        }
        
        if (!mecha2.estasVivo()) {
