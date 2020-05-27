4ebcd740658f5eba9f785e052497c25ace1bab22
Agustin Paoletti <agustinpaoletti92@outlook.com>
2020-05-26 21:48:59 -0300

    Shield añadido #7

--- src/mechadecorator/ShieldDefense.java
+++ src/mechadecorator/ShieldDefense.java
@@ -5,6 +5,8 @@
  */
 package mechadecorator;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  *
  * @author Smoker
@@ -12,6 +14,13 @@ package mechadecorator;
 public class ShieldDefense implements IDefense {
     @Override
     public int defend(int damage) {
+        int randomNum = ThreadLocalRandom.current().nextInt(0, 100 + 1);
+        
+        if (randomNum <= 5) {
+            System.out.println("Se ha cubierto rapidamente con el escudo y recibe la mitad de daño");
+            return Math.max(0, (damage - this.def()) / 2);
+        }
+        
         return Math.max(0, damage - this.def());
     }
 
